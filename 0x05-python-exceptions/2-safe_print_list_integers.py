9198df0667a4e9394091d9903e8315960b0ddbd3
Kagiso Motlhaoleng <kagisokayman@gmail.com>
2023-11-20 20:53:05 +0200

    Removed another try expression

--- 0x05-python-exceptions/2-safe_print_list_integers.py
+++ 0x05-python-exceptions/2-safe_print_list_integers.py
@@ -1,14 +1,12 @@
 #!/usr/bin/python3
 def safe_print_list_integers(my_list=[], x=0):
     int_printed = 0
-    try:
-        for item in range(x):
+    for item in range(x):
             try:
                 print("{:d}".format(item), end=' ')
                 int_printed += 1
             except (ValueError, TypeError):
                 pass
-        print()
-    except TypeError:
-        return int_printed
+    print()
+    return int_printed
     
