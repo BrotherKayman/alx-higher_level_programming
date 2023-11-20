8c1590b89abd9543014f01152ebf3ac70d374160
Kagiso Motlhaoleng <kagisokayman@gmail.com>
2023-11-20 20:12:01 +0200

    Remove str.format

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -3,7 +3,7 @@ def safe_print_list(my_list=[], x=0):
     int_printed = 0
     try:
         for index in range(x):
-            print("{}".format(my_list[index]), end="")
+            print(my_list[index], end="")
             int_printed += 1
     except IndexError:
         pass
