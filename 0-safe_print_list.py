28e5dc6d51e15090807d5047600f076b358fc06e
Kagiso Motlhaoleng <kagisokayman@gmail.com>
2023-11-20 19:50:23 +0200

    Modified

--- 0-safe_print_list.py
+++ 0-safe_print_list.py
@@ -1,11 +1,10 @@
-afe_print_list(my_list=[], x=0):
-        int_printed = 0
-            try:
-                        for index in range(x):
-                                        print("{}".format(my_list[index]), end="")
-                                                    int_printed += 1
-                                                        except IndexError:
-                                                                    pass
-                                                                    print('\n')
-                                                                        return int_printed
-
+def safe_print_list(my_list=[], x=0):
+    int_printed = 0
+    try:
+        for index in range(x):
+            print("{}".format(my_list[index]), end="")
+            int_printed += 1
+    except IndexError:
+        pass
+        print('\n')
+        return int_printed
