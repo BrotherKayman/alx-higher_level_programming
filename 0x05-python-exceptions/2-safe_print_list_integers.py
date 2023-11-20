a8fbc1aaeef70fa6009781e2b3fbc60a7d590d42
Kagiso Motlhaoleng <kagisokayman@gmail.com>
2023-11-20 20:29:03 +0200

    Print first x element

---
+++ 0x05-python-exceptions/2-safe_print_list_integers.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+def safe_print_list_integers(my_list=[], x=0):
+    int_printed = 0
+    try:
+        for item in my_list[:x]:
+            try:
+                print("{:d}".format(item), end=' ')
+                int_printed += 1
+            except ValueError:
+                pass
+        print()
+    except TypeError:
+        return int_printed
+    
