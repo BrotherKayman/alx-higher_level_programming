49f217432dfca536717fea4dffb7585c5df1b176
Kagiso Motlhaoleng <kagisokayman@gmail.com>
2023-12-12 23:07:55 +0200

    Square update

--- 0x0C-python-almost_a_circle/models/square.py
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -10,7 +10,6 @@ class Square(Rectangle):
     Square class that inherits from Rectangle.
     """
 
-
     def __init__(self, size, x=0, y=0, id=None):
         """
         Initializes a Square.
@@ -22,11 +21,67 @@ class Square(Rectangle):
             id (int): Identifier (default is None).
         """
         super().__init__(size, size, x, y, id)
-    def __str__(self):
+
+    @property
+    def size(self):
         """
-        Returns a string representation of the Square.
+        Gets the size of the square.
 
         Returns:
-            str: String representation of the Square.
+            int: Size of the square.
         """
-        return f"[Square] ({self.id}) {self.x}/{self.y} - {self.width}"
+        return self.width
+
+    @size.setter
+    def size(self, value):
+        """
+        Sets the size of the square.
+
+        Args:
+            value (int): New size for the square.
+        """
+        self.width = value
+        self.height = value
+
+    def __update(self, id=None, size=None, x=None, y=None):
+        """
+        Internal method that updates instance attributes via *args and **kwargs.
+
+        Args:
+            id (int): New identifier value (default is None).
+            size (int): New size value (default is None).
+            x (int): New x-coordinate value (default is None).
+            y (int): New y-coordinate value (default is None).
+        """
+        if id is not None:
+            self.id = id
+        if size is not None:
+            self.width = size
+            self.height = size
+        if x is not None:
+            self.x = x
+        if y is not None:
+            self.y = y
+
+    def update(self, *args, **kwargs):
+        """
+        Updates instance attributes via positional and keyword arguments.
+
+        Args:
+            *args: Positional arguments for id, size, x, y.
+            **kwargs: Keyword arguments for id, size, x, y.
+        """
+        if args:
+            self.__update(*args)
+        elif kwargs:
+            self.__update(**kwargs)
+
+    def to_dictionary(self):
+        """
+        Returns a dictionary representation of the Square object.
+
+        Returns:
+            dict: Dictionary containing id, size, x, and y.
+        """
+        return {"id": self.id, "width": self.__width, "height": self.__height,
+                "x": self.__x, "y": self.__y}
